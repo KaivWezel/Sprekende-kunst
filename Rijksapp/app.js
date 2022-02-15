91e90e558e9a191713022f7ec8d36efd74b9006f
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-02-15 22:09:11 +0100

    started query generator for fetch

--- Rijksapp/app.js
+++ Rijksapp/app.js
@@ -1,7 +1,24 @@
 console.log("check");
 
+//URL variables
 const url = `https://www.rijksmuseum.nl/api/nl/collection?key=vnkVfTSO&involvedMaker=Rembrandt+van+Rijn`;
+const hostURL = new URL(url).hostname;
+const pathURL = new URL(url).pathname;
+const apiKey = "vnkVfTSO";
+
+//DOM variables
 const results = document.querySelector("ul");
+const cultSelect = document.querySelector("#culture");
+let culture = cultSelect.value;
+const searchForm = document.querySelector(".searchForm");
+
+searchForm.onsubmit = function (e) {
+	e.preventDefault();
+	console.log("submitted form");
+	getData();
+};
+
+cultSelect.onchange = setCulture;
 
 fetch(url)
 	.then(function (response) {
@@ -19,4 +36,35 @@ function getAndRenderArt(artList) {
 
 		results.appendChild(artCard);
 	});
+}
+
+function setCulture() {
+	culture = cultSelect.value;
+	console.log(culture);
+}
+
+function getData() {
+	const queryURL = defineQuery();
+	const endpoint = generateEndpoint(queryURL);
+	console.log(endpoint);
+}
+
+function defineQuery() {
+	const queries = document.querySelectorAll(".searchForm input");
+	let queryString = [];
+	queries.forEach(function (query) {
+		if (query.value.length != 0) {
+			queryString.push(`${query.name}=${query.value}`);
+		}
+		console.log(queryString);
+		query.value = "";
+	});
+	const finalURL = queryString.join("&").replace(/\s+/g, "+");
+	console.log(finalURL);
+	return finalURL;
+}
+
+function generateEndpoint(query) {
+	const endpoint = `${hostURL}${pathURL}?key=${apiKey}&${query}`;
+	return endpoint;
 }
