56c8434ead80f80abc5c466787a5592c0cbd2861
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-02-15 11:59:44 +0100

    Added fetch function

--- Rijksapp/app.js
+++ Rijksapp/app.js
@@ -1,1 +1,22 @@
 console.log("check");
+
+const url = `https://www.rijksmuseum.nl/api/nl/collection?key=vnkVfTSO&involvedMaker=Rembrandt+van+Rijn`;
+const results = document.querySelector("ul");
+
+fetch(url)
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data.artObjects);
+		getAndRenderArt(data.artObjects);
+	});
+
+function getAndRenderArt(artList) {
+	artList.forEach((art) => {
+		const artCard = document.createElement("li");
+		artCard.innerText = art.title;
+
+		results.appendChild(artCard);
+	});
+}
