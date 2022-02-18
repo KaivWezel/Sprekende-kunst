c49939f2e7ee195882b9bd16c3e32868b8e82eb2
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-02-18 14:58:14 +0100

    Finished getData

--- Rijksapp/app.js
+++ Rijksapp/app.js
@@ -12,59 +12,96 @@ const cultSelect = document.querySelector("#culture");
 let culture = cultSelect.value;
 const searchForm = document.querySelector(".searchForm");
 
-searchForm.onsubmit = function (e) {
+searchForm.onsubmit = async function (e) {
 	e.preventDefault();
 	console.log("submitted form");
-	getData();
+
+	const artworks = await getData();
+	console.log("results", artworks);
+	artworks.forEach((art) => {
+		getDetails(art);
+	});
+	console.log();
 };
 
-cultSelect.onchange = setCulture;
+// get data
+// get details
 
-fetch(url)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (data) {
-		console.log(data.artObjects);
-		getAndRenderArt(data.artObjects);
-	});
+getData();
 
-function getAndRenderArt(artList) {
-	artList.forEach((art) => {
-		const artCard = document.createElement("li");
-		artCard.innerText = art.title;
+// cultSelect.onchange = setCulture;
 
-		results.appendChild(artCard);
-	});
-}
-
+// Set language
 function setCulture() {
 	culture = cultSelect.value;
 	console.log(culture);
 }
 
-function getData() {
+async function getData() {
+	// constructing query
 	const queryURL = defineQuery();
+	// constructing endpoint useing query
 	const endpoint = generateEndpoint(queryURL);
+	// Fetch to endpoint
+	const artworks = await fetch(endpoint)
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (data) {
+			return data.artObjects;
+		});
+	return artworks;
+}
+
+async function getDetails(art) {
+	console.log(art);
+	// Filter id
+	const filteredId = art.id.split("").slice(3).join("");
+	// Insert id into detail URL
+	const endpoint = `https://${hostURL}${pathURL}/${filteredId}?key=${apiKey}`;
 	console.log(endpoint);
+	// Fetch details
+	const details = await fetch(endpoint)
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (data) {
+			return data;
+		});
+	return details;
 }
 
 function defineQuery() {
+	// Defining inputs for query
 	const queries = document.querySelectorAll(".searchForm input");
+	// Declaring queryString for storage
 	let queryString = [];
+	// Reading input value
 	queries.forEach(function (query) {
-		if (query.value.length != 0) {
+		if (query.value.length > 0) {
+			// Store input value in queryString
 			queryString.push(`${query.name}=${query.value}`);
 		}
-		console.log(queryString);
 		query.value = "";
 	});
-	const finalURL = queryString.join("&").replace(/\s+/g, "+");
-	console.log(finalURL);
-	return finalURL;
+	// Constructing final query
+	const finalQuery = queryString.join("&").replace(/\s+/g, "+");
+	return finalQuery;
 }
 
 function generateEndpoint(query) {
-	const endpoint = `${hostURL}${pathURL}?key=${apiKey}&${query}`;
+	const endpoint = `https://${hostURL}${pathURL}?key=${apiKey}&${query}`;
 	return endpoint;
 }
+
+function renderCards() {
+	// Create card element
+	const card = document.createElement("div");
+	card.classList.add("artCard");
+	const image = document.createElement("img");
+	const title = document.createElement("h3");
+	// Assign content to elements
+	// Append to list
+}
+
+renderCards();
