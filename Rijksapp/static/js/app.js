d8ad29909fa7d7dc288809fe45930d5a431e921d
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-01 11:10:37 +0100

    started pagination

--- Rijksapp/static/js/app.js
+++ Rijksapp/static/js/app.js
@@ -3,14 +3,15 @@ import * as ui from "./modules/ui.js";
 console.log("check");
 
 //DOM variables
-const results = document.querySelector("ul");
+const results = document.querySelector(".results");
 const cultSelect = document.querySelector("#culture");
 let culture = cultSelect.value;
 const searchForm = document.querySelector(".searchForm");
+const nextBtn = document.querySelector(".nextPage");
 
 searchForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	ui.createList(e);
+	ui.renderResult();
 });
 
 // Set language
@@ -18,3 +19,14 @@ function setCulture() {
 	culture = cultSelect.value;
 	console.log(culture);
 }
+
+nextBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	ui.renderResult();
+});
+
+// Nextbutton pressed
+// increase pagination for api
+// fetch next results
+// create cards for next results
+//
