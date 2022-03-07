4639e37aa3cd4b1dd5c1c2e93af4bc5a05d7f7df
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-07 12:14:24 +0100

    Created routes + render function

--- Rijksapp/static/js/modules/router.js
+++ Rijksapp/static/js/modules/router.js
@@ -1,9 +1,24 @@
-const router = (route) => {
-	switch (key) {
-		case value:
-			break;
+import { renderPage } from "./ui.js";
 
+// In principe kan deze functie ook in de hashchange, maar dit is om te laten zien dat ik het kan gebruiken.
+export default () => {
+	switch (location.hash) {
+		case "#home":
+			clearPage();
+			renderPage(location.hash);
+		case "#search":
+			clearPage();
+			renderPage(location.hash);
+			break;
+		case "#results":
+			clearPage();
+			renderPage(location.hash);
 		default:
 			break;
 	}
+};
+
+const clearPage = () => {
+	const pages = document.querySelectorAll("section");
+	pages.forEach((page) => page.classList.remove("activePage"));
 };
